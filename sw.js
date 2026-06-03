cffbe99a735899272d34b3f344136f870bb5d878
learningwithsamara-web <learningwithsamara@gmail.com>
2026-06-03 10:14:46 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // LWS Media Network — Service Worker
-// Version: lws-media-v1.0.10
+// Version: lws-media-v1.0.11
 
-var CACHE_NAME = 'lws-media-v1.0.10';
+var CACHE_NAME = 'lws-media-v1.0.11';
 
 var STATIC_ASSETS = [
   '/',
